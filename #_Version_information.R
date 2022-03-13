ecbadad7b8aa0c72c9864ebf04e34c80b1bf4d6c
Charlene717 <q2330416@gmail.com>
2022-03-13 20:28:04 +0800

    Ori

---
+++ #_Version_information.R
@@ -0,0 +1,61 @@
+## version
+##### Version information ######  
+  # platform       x86_64-w64-mingw32          
+  # arch           x86_64                      
+  # os             mingw32                     
+  # system         x86_64, mingw32             
+  # status                                     
+  # major          4                           
+  # minor          1.2                         
+  # year           2021                        
+  # month          11                          
+  # day            01                          
+  # svn rev        81115                       
+  # language       R                           
+  # version.string R version 4.1.2 (2021-11-01)
+  # nickname       Bird Hippie 
+  #--------------------------------#
+  # R version 4.1.2
+  # Seurat 4.0.2
+
+
+## sessionInfo()
+##### R Package Version Information #####
+  # loaded via a namespace (and not attached):
+  #   [1] plyr_1.8.6                    igraph_1.2.11                 lazyeval_0.2.2                splines_4.1.2                
+  # [5] BiocParallel_1.28.3           listenv_0.8.0                 scattermore_0.8               digest_0.6.29                
+  # [9] htmltools_0.5.2               fansi_0.5.0                   memoise_2.0.1                 tensor_1.5                   
+  # [13] cluster_2.1.2                 ROCR_1.0-11                   globals_0.14.0                Biostrings_2.62.0            
+  # [17] matrixStats_0.61.0            spatstat.sparse_2.1-0         prettyunits_1.1.1             colorspace_2.0-2             
+  # [21] blob_1.2.2                    rappdirs_0.3.3                ggrepel_0.9.1                 xfun_0.29                    
+  # [25] crayon_1.5.0                  RCurl_1.98-1.6                jsonlite_1.7.3                spatstat.data_2.1-2          
+  # [29] survival_3.2-13               zoo_1.8-9                     glue_1.5.0                    polyclip_1.10-0              
+  # [33] gtable_0.3.0                  zlibbioc_1.40.0               XVector_0.34.0                leiden_0.3.9                 
+  # [37] DelayedArray_0.20.0           future.apply_1.8.1            abind_1.4-5                   scales_1.1.1                 
+  # [41] DBI_1.1.2                     spatstat.random_2.1-0         miniUI_0.1.1.1                Rcpp_1.0.7                   
+  # [45] progress_1.2.2                viridisLite_0.4.0             xtable_1.8-4                  reticulate_1.24              
+  # [49] spatstat.core_2.4-0           bit_4.0.4                     htmlwidgets_1.5.4             httr_1.4.2                   
+  # [53] RColorBrewer_1.1-2            ellipsis_0.3.2                ica_1.0-2                     XML_3.99-0.9                 
+  # [57] pkgconfig_2.0.3               uwot_0.1.11                   deldir_1.0-6                  utf8_1.2.2                   
+  # [61] tidyselect_1.1.2              rlang_1.0.1                   reshape2_1.4.4                later_1.3.0                  
+  # [65] munsell_0.5.0                 BiocVersion_3.14.0            tools_4.1.2                   cachem_1.0.6                 
+  # [69] cli_3.1.0                     generics_0.1.2                RSQLite_2.2.9                 ggridges_0.5.3               
+  # [73] fastmap_1.1.0                 yaml_2.2.2                    goftest_1.2-3                 bit64_4.0.5                  
+  # [77] fitdistrplus_1.1-6            purrr_0.3.4                   RANN_2.6.1                    KEGGREST_1.34.0              
+  # [81] pbapply_1.5-0                 future_1.24.0                 nlme_3.1-153                  mime_0.12                    
+  # [85] xml2_1.3.3                    biomaRt_2.50.3                compiler_4.1.2                rstudioapi_0.13              
+  # [89] plotly_4.10.0                 filelock_1.0.2                curl_4.3.2                    png_0.1-7                    
+  # [93] interactiveDisplayBase_1.32.0 spatstat.utils_2.3-0          tibble_3.1.6                  stringi_1.7.6                
+  # [97] lattice_0.20-45               ProtGenerics_1.26.0           Matrix_1.3-4                  vctrs_0.3.8                  
+  # [101] pillar_1.7.0                  lifecycle_1.0.1               BiocManager_1.30.16           spatstat.geom_2.3-2          
+  # [105] lmtest_0.9-39                 RcppAnnoy_0.0.19              data.table_1.14.2             cowplot_1.1.1                
+  # [109] bitops_1.0-7                  irlba_2.3.5                   rtracklayer_1.54.0            httpuv_1.6.5                 
+  # [113] BiocIO_1.4.0                  R6_2.5.1                      promises_1.2.0.1              KernSmooth_2.23-20           
+  # [117] gridExtra_2.3                 parallelly_1.30.0             codetools_0.2-18              MASS_7.3-54                  
+  # [121] assertthat_0.2.1              SummarizedExperiment_1.24.0   rjson_0.2.21                  withr_2.4.3                  
+  # [125] GenomicAlignments_1.30.0      Rsamtools_2.10.0              sctransform_0.3.3             GenomeInfoDbData_1.2.7       
+  # [129] hms_1.1.1                     mgcv_1.8-38                   parallel_4.1.2                grid_4.1.2                   
+  # [133] rpart_4.1-15                  tidyr_1.1.4                   MatrixGenerics_1.6.0          Rtsne_0.15                   
+  # [137] shiny_1.7.1                   tinytex_0.37                  restfulr_0.0.13
+
+
